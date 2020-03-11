fd027499534b448338ab27cc90d6b0d00ab00a6b
oscar <oscar@kleverness.com>
2020-03-11 14:46:08 -0600

    Insert User

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,6 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -16,7 +15,7 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                   Bienvenido {{ Auth::user()->name }} {{ Auth::user()->last_name_dad }}
                 </div>
             </div>
         </div>
